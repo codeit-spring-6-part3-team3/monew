39ea4dc1af053c853cacd98bceadbdaf73fc3e42
이치호 <ic471544@gmail.com>
2025-11-25 16:40:19 +0900

    [이치호] feat : package 구조 초기 생성

---
+++ src/main/java/com/team03/monew/notification/controller/NotificationController.java
@@ -0,0 +1,4 @@
+package com.team03.monew.notification.controller;
+
+public class NotificationController {
+}
---
+++ src/main/java/com/team03/monew/notification/dto/NotificationDto.java
@@ -0,0 +1,4 @@
+package com.team03.monew.notification.dto;
+
+public class NotificationDto {
+}
