c91db5e08fe1b0dfa0aac4176eb63f8a0a99d486
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-26 15:43:04 +0900

    [박지훈] feat : subscribeCount, 구독자수 추가, 삭제 메소드 추가

--- src/main/java/com/team03/monew/interest/domain/Interest.java
+++ src/main/java/com/team03/monew/interest/domain/Interest.java
@@ -27,6 +27,8 @@ public class Interest {
     @Column(columnDefinition = "varchar(100)[]")
     private List<String> keywords;
 
+    @Column(length = 100)
+    private int subscribeCount;
     @CreatedDate
     private Instant createdAt;
 
@@ -39,16 +41,19 @@ public class Interest {
     public Interest(String name, List<String> keywords) {
         this.name = name;
         this.keywords = keywords;
+        this.subscribeCount = 0;
     }
 
     void keywordAdd(String keyword) {
         this.keywords.add(keyword);
     }
 
-    void keywordRemove(String keyword) {
-        this.keywords.remove(keyword);
-    }
+    void keywordRemove(String keyword) { this.keywords.remove(keyword); }
 
+    void subscribeAdd() {this.subscribeCount = this.subscribeCount + 1; }
+
+    void subscribeRemove() {this.subscribeCount = this.subscribeCount - 1; }
+    
     //관심사 유사도 검사 메소드
     boolean nameEquals(String name) {
         int nameLength = this.name.length();
