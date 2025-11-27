dd58e0b70600d831d024e648fc9ac7accaef107c
이치호 <ic471544@gmail.com>
2025-11-27 14:26:42 +0900

    [이치호] feat : 댓글-좋아요 도메인, 엔티티 생성

--- src/main/java/com/team03/monew/common/domain/CommentLike.java
+++ src/main/java/com/team03/monew/common/domain/CommentLike.java
@@ -1,12 +1,18 @@
-package com.team03.monew.common;
+package com.team03.monew.common.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Table(name = "commentLikes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentLike {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -22,4 +28,18 @@ public class CommentLike {
     @Column(name = "userId", nullable = false)
     private UUID userId;
 
+    private CommentLike(
+            UUID commentId,
+            UUID userId
+    ) {
+        this.commentId = commentId;
+        this.userId = userId;
+    }
+
+    public static CommentLike create(
+            UUID commentId,
+            UUID userId
+    ) {
+        return new CommentLike(commentId, userId);
+    }
 }
