5708f4f32ecf72d8163e92cd14c4b28941eac158
Hyunwook <hjhsha827@gmail.com>
2025-11-28 18:07:23 +0900

    [이현욱] feat: 뉴스 수집 서비스 추가 및 스케줄러 연동 구조 개선

--- src/main/java/com/team03/monew/common/scheduler/NewsRssScheduler.java
+++ src/main/java/com/team03/monew/common/scheduler/NewsRssScheduler.java
@@ -1,11 +1,14 @@
 package com.team03.monew.common.scheduler;
 
+import com.team03.monew.common.service.NewsCollectService;
 import org.springframework.scheduling.annotation.Scheduled;
 
 public class NewsRssScheduler {
 
+  NewsCollectService newsCollectService;
+
   @Scheduled(cron = "0 0 * * * *")
   public void collectNews() {
-
+    newsCollectService.fetchAllNews();
   }
 }
