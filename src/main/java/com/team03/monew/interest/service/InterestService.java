3d4360c3ceb65331a70f996f13bfa843d8c7384d
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-28 19:50:37 +0900

    [박지훈] feat :InterestService에 interestUpdate메소드 추가

--- src/main/java/com/team03/monew/interest/service/InterestService.java
+++ src/main/java/com/team03/monew/interest/service/InterestService.java
@@ -3,8 +3,14 @@ package com.team03.monew.interest.service;
 
 import com.team03.monew.interest.dto.InterestDto;
 import com.team03.monew.interest.dto.InterestRegisterRequest;
+import com.team03.monew.interest.dto.InterestUpdateRequest;
+
+import java.rmi.NoSuchObjectException;
+import java.util.UUID;
 
 public interface InterestService {
 
     InterestDto interestCreate(InterestRegisterRequest request);
+
+    InterestDto interestUpdate(UUID interest, InterestUpdateRequest request) throws NoSuchObjectException;
 }
