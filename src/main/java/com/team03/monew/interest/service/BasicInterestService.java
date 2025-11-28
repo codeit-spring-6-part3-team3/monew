bdb00e0a83696fff0223cbc6f190704b9dbff96c
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-28 19:50:37 +0900

    [박지훈] feat :InterestService에 interestUpdate메소드 추가

--- src/main/java/com/team03/monew/interest/service/BasicInterestService.java
+++ src/main/java/com/team03/monew/interest/service/BasicInterestService.java
@@ -4,6 +4,7 @@ import com.sun.jdi.request.DuplicateRequestException;
 import com.team03.monew.interest.domain.Interest;
 import com.team03.monew.interest.dto.InterestDto;
 import com.team03.monew.interest.dto.InterestRegisterRequest;
+import com.team03.monew.interest.dto.InterestUpdateRequest;
 import com.team03.monew.interest.mapper.InterestMapper;
 import com.team03.monew.interest.repository.InterestRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+
+import java.rmi.NoSuchObjectException;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -44,4 +48,16 @@ public class BasicInterestService implements InterestService {
 
         return interestMapper.toDto(interest,null);
     }
+
+    @Override
+    public InterestDto interestUpdate(UUID interest,InterestUpdateRequest request) throws NoSuchObjectException {
+        Interest interestUpdate = interestRepository.findById(interest)
+                .orElseThrow(() -> new NoSuchObjectException("해당 관심사 없음"));
+
+        interestUpdate.keywordUpdate(request.keywords());
+
+        interestRepository.save(interestUpdate);
+        return interestMapper.toDto(interestUpdate,null);
+    }
+
 }
