f64466fcc6d141e4ff7e9e81affeb730a9d1b8c2
SeoGyeongWon <skwknh00@naver.com>
2025-12-01 01:37:52 +0900

    [서경원] Feat: 뉴스 등록 기능 서비스 메서드 구현

--- src/main/java/com/team03/monew/news/service/BasicNewsService.java
+++ src/main/java/com/team03/monew/news/service/BasicNewsService.java
@@ -1,5 +1,40 @@
 package com.team03.monew.news.service;
 
-public class BasicNewsService {
+import com.team03.monew.news.domain.News;
+import com.team03.monew.news.dto.NewsCreateRequest;
+import com.team03.monew.news.dto.NewsResponseDto;
+import com.team03.monew.news.exception.CustomException.SameResourceLink;
+import com.team03.monew.news.repository.NewsRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
+public class BasicNewsService implements NewsService {
+
+  private final NewsRepository newsRepository;
+
+  @Override
+  public NewsResponseDto createNews(NewsCreateRequest newsCreateRequest) {
+
+    Optional<News> existing = newsRepository.findByResourceLink(newsCreateRequest.resourceLink());
+    if(existing.isPresent()) {
+      throw new SameResourceLink();
+    }
+    //엔티티
+    News news = News.builder()
+        .source(newsCreateRequest.source())
+        .resourceLink(newsCreateRequest.resourceLink())
+        .title(newsCreateRequest.title())
+        .postDate(newsCreateRequest.postDate())
+        .overview(newsCreateRequest.overView())
+        .build();
+
+    //저장
+    News savedNews = newsRepository.save(news);
+
+    //dto변환
+    return NewsResponseDto.from(savedNews);
+  }
 }
