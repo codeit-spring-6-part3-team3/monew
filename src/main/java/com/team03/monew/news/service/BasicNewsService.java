42657e6d99adaf79108c643104cc4c0d45389309
SeoGyeongWon <skwknh00@naver.com>
2025-12-02 14:18:08 +0900

    [서경원] Feat: 뉴스 삭제 서비스 구현

--- src/main/java/com/team03/monew/news/service/BasicNewsService.java
+++ src/main/java/com/team03/monew/news/service/BasicNewsService.java
@@ -2,12 +2,16 @@ package com.team03.monew.news.service;
 
 import com.team03.monew.news.domain.News;
 import com.team03.monew.news.dto.NewsCreateRequest;
+import com.team03.monew.news.dto.NewsDeleteRequest;
 import com.team03.monew.news.dto.NewsResponseDto;
+import com.team03.monew.news.exception.CustomException.NewsCanNotDelete;
+import com.team03.monew.news.exception.CustomException.NewsNotFound;
 import com.team03.monew.news.exception.CustomException.SameResourceLink;
 import com.team03.monew.news.repository.NewsRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,6 +19,7 @@ public class BasicNewsService implements NewsService {
 
   private final NewsRepository newsRepository;
 
+  //뉴스 저장(등록)
   @Override
   public NewsResponseDto createNews(NewsCreateRequest newsCreateRequest) {
 
@@ -36,5 +41,29 @@ public class BasicNewsService implements NewsService {
 
     //dto변환
     return NewsResponseDto.from(savedNews);
+  }
+
+  // 뉴스 삭제(논리)
+  @Transactional
+  @Override
+  public void deleteNews_logical(NewsDeleteRequest newsDeleteRequest) {
+    News news = newsRepository.findById(newsDeleteRequest.articleId())
+        .orElseThrow(NewsNotFound::new);
+
+    news.deleteNews();
+    newsRepository.save(news);
+  }
+
+  //뉴스 삭제(물리)
+  @Transactional
+  @Override
+  public void deleteNews_physical(NewsDeleteRequest newsDeleteRequest) {
+    News news = newsRepository.findById(newsDeleteRequest.articleId())
+        .orElseThrow(NewsNotFound::new);
+
+    if(!news.isDelete()){
+      throw new NewsCanNotDelete();
+    }
+    newsRepository.delete(news);
   }
 }
