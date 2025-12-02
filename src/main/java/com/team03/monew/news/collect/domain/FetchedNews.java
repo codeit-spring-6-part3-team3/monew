70d7cbcbbe5576ff6fa682f4a23722265c487049
Hyunwook <hjhsha827@gmail.com>
2025-12-02 14:47:54 +0900

    [이현욱] refactor: FetchNews Class -> record로 수정

--- src/main/java/com/team03/monew/news/collect/domain/FetchedNews.java
+++ src/main/java/com/team03/monew/news/collect/domain/FetchedNews.java
@@ -3,16 +3,15 @@ package com.team03.monew.news.collect.domain;
 import com.team03.monew.news.domain.NewsSourceType;
 import java.time.LocalDateTime;
 import lombok.Builder;
-import lombok.RequiredArgsConstructor;
 
 @Builder
-@RequiredArgsConstructor
-public class FetchedNews {
-  private final NewsSourceType source;
-  private final String resourceLink;
-  private final String title;
-  private final LocalDateTime postDate;
-  private final String overview;
+public record FetchedNews(
+    NewsSourceType source,
+    String resourceLink,
+    String title,
+    LocalDateTime postDate,
+    String overview
+) {
 
   @Override
   public String toString() {
