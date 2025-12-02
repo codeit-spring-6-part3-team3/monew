f73a1bb1b82bb5a57730f53b08c4c8b5590b5d43
jihoon-68 <pkrwlgns64@gmail.com>
2025-12-02 13:55:59 +0900

    [박지훈] feat : interest service interestDelete 추가

--- src/main/java/com/team03/monew/interest/service/BasicInterestService.java
+++ src/main/java/com/team03/monew/interest/service/BasicInterestService.java
@@ -7,6 +7,8 @@ import com.team03.monew.interest.dto.InterestRegisterRequest;
 import com.team03.monew.interest.dto.InterestUpdateRequest;
 import com.team03.monew.interest.mapper.InterestMapper;
 import com.team03.monew.interest.repository.InterestRepository;
+import com.team03.monew.subscribe.domain.Subscribe;
+import com.team03.monew.subscribe.repository.SubscribeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,6 +16,7 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import java.rmi.NoSuchObjectException;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -22,6 +25,7 @@ import java.util.UUID;
 public class BasicInterestService implements InterestService {
 
     private InterestRepository interestRepository;
+    private SubscribeRepository subscribeRepository;
     private InterestMapper interestMapper;
 
     @Override
@@ -58,6 +62,18 @@ public class BasicInterestService implements InterestService {
 
         interestRepository.save(interestUpdate);
         return interestMapper.toDto(interestUpdate,null);
+    }
+
+    public void interestDelete(UUID interest) throws NoSuchObjectException {
+        Interest interestDelete = interestRepository.findById(interest)
+                .orElseThrow(()-> new NoSuchObjectException("관심사 정보 없음"));
+
+        List<Subscribe> subscribeList = subscribeRepository.findAll().stream()
+                .filter(subscribe -> subscribe.getInterestId().equals(interestDelete.getId()))
+                .toList();
+        subscribeRepository.deleteAll(subscribeList);
+
+        interestRepository.delete(interestDelete);
     }
 
 }
