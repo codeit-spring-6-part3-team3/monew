6c1003b7a6c20df993ee5b955fd9deef7cecdb47
SeoGyeongWon <skwknh00@naver.com>
2025-12-02 16:26:06 +0900

    [서경원] Refactor: @Prepersist를 @CreatedDate로변경, 사용하지 않는 import문 삭제, 칼럼 크기 지정

--- src/main/java/com/team03/monew/news/domain/News.java
+++ src/main/java/com/team03/monew/news/domain/News.java
@@ -7,8 +7,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import java.time.LocalDateTime;
@@ -17,12 +15,14 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 //jspecify의 nullmarked 사용하여 별도의 null여부 없으면 모두 nonNull로 간주
+// reosourceLink에 유니크 제약 조건을 걸어둠
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-// reosourceLink에 유니크 제약 조건을 걸어둠
 @Table(name = "news",
   uniqueConstraints = {
     @UniqueConstraint(columnNames = "resourceLink")
@@ -38,25 +38,27 @@ public class News {
   @Column(name = "source")
   private NewsSourceType source;
 
-  @Column(name = "resourceLink")
+  @Column(name = "resourceLink",length = 300)
   private String resourceLink;
 
-  @Column(name= "title")
+  @Column(name= "title", length = 100)
   private String title;
 
   @Column(name = "postDate")
   private LocalDateTime postDate;
 
-  @Column(name = "overview")
+  @Column(name = "overview", length = 1000)
   private String overview;
 
   @Column(name = "viewCount",columnDefinition = "INTEGER DEFAULT 0")
   private int viewCount;
 
   @Column(name = "creationAt", updatable = false)
+  @CreatedDate
   private LocalDateTime createdAt;
 
   @Column(name = "updatedAt",nullable = true)
+  @LastModifiedDate
   private LocalDateTime updatedAt;
 
   @Column(name = "isDelete",columnDefinition = "boolean default false")
@@ -82,20 +84,9 @@ public class News {
 
   // 읽은 수 증가
   public void increaseViewCount(){
-
   }
 
-  @PrePersist
-  public void onCreate() {
-    this.createdAt = LocalDateTime.now();
-    this.updatedAt = LocalDateTime.now();
-  }
-
-  @PreUpdate
-  public void onUpdate() {
-    this.updatedAt = LocalDateTime.now();
-  }
-
+  // 뉴스 논리 삭제
   public void deleteNews(){
     this.isDelete = true;
   }
