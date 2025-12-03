64125b4fc9a917dc33903b04cfd363d4a25264d1
jihoon-68 <pkrwlgns64@gmail.com>
2025-12-03 11:22:13 +0900

    [박지훈] feat : InterestRepository에 커서 페이지네이션 추가

--- src/main/java/com/team03/monew/interest/repository/InterestRepository.java
+++ src/main/java/com/team03/monew/interest/repository/InterestRepository.java
@@ -4,5 +4,5 @@ import com.team03.monew.interest.domain.Interest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
-public interface InterestRepository extends JpaRepository<Interest, UUID> {
+public interface InterestRepository extends JpaRepository<Interest, UUID>, InterestRepositoryCustom {
 }
