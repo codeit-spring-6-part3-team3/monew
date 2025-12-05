86a64dc83aecce3fa5e3e7bce882e05dcba8b160
chiho0270 <96578548+chiho0270@users.noreply.github.com>
2025-12-05 17:24:21 +0900

    파라미터 디티오로 전환하고 디티오를 안채움;; ㅎ

--- src/main/java/com/team03/monew/comment/dto/CursorPageRequestCommentDto.java
+++ src/main/java/com/team03/monew/comment/dto/CursorPageRequestCommentDto.java
@@ -1,4 +1,15 @@
 package com.team03.monew.comment.dto;
 
-public record CursorPageRequestCommentDto() {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CursorPageRequestCommentDto(
+        UUID articleId,
+        String orderBy,
+        String direction,
+        String cursor,
+        LocalDateTime after,
+        Integer limit,
+        UUID userId
+) {
 }
