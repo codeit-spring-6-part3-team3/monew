42b9a40ad4f8640da17f3db6127948466d94a542
SeoGyeongWon <skwknh00@naver.com>
2025-12-08 09:42:17 +0900

    [서경원] Feat: 컨트롤러 뉴스 등록 추가

--- src/main/java/com/team03/monew/news/controller/NewsController.java
+++ src/main/java/com/team03/monew/news/controller/NewsController.java
@@ -2,7 +2,9 @@ package com.team03.monew.news.controller;
 
 import com.team03.monew.news.domain.NewsSourceType;
 import com.team03.monew.news.dto.CursorPageResponseArticleDto;
+import com.team03.monew.news.dto.NewsCreateRequest;
 import com.team03.monew.news.dto.NewsDto;
+import com.team03.monew.news.dto.NewsResponseDto;
 import com.team03.monew.news.service.NewsService;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,6 +12,8 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class NewsController {
 
   private final NewsService newsService;
+
+  // 뉴스 등록
+  @PostMapping
+  public NewsResponseDto createNews(
+      @RequestBody NewsCreateRequest newsCreateRequest,
+      @RequestParam UUID interestId
+      ) {
+    return newsService.createNews(newsCreateRequest, interestId);
+  }
 
   // 뉴스 목록 조회
   @GetMapping
