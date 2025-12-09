f3f008e334037f5d6e0d9ff139896da34e4f12f9
chiho0270 <96578548+chiho0270@users.noreply.github.com>
2025-12-09 23:30:00 +0900

    [이치호] feat : 댓글좋아요 관리, 활동내역 최근 좋아요 조회

--- src/main/java/com/team03/monew/commentLike/repository/CommentLikeRepository.java
+++ src/main/java/com/team03/monew/commentLike/repository/CommentLikeRepository.java
@@ -7,15 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CommentLikeRepository extends JpaRepository<CommentLike, UUID> {
 
-    // 최근 좋아요 조회 (최대 10개) ... 추신 습관적으로 해버렸습니다. 죄송합니다.
-    @Modifying
-    @Query("SELECT c FROM CommentLike c ORDER BY c.creationAt DESC LIMIT 10")
-    CommentLike findByCommentIdAndUserId(
+    // 최근 좋아요 조회 (최대 10개)
+    @Query("SELECT cl FROM CommentLike cl WHERE cl.userId = :userId ORDER BY cl.creationAt DESC LIMIT 10")
+    List<CommentLike> findTopTenByUserIdOrderByCreationAtDesc(
             @Param("id") UUID commentId,
             @Param("userId") UUID userId
     );
