73da197e6c61a52fa12744edfc9dd8dfdf8abed3
jihoon-68 <pkrwlgns64@gmail.com>
2025-12-10 18:07:18 +0900

    [박지훈] feat : SubscribeRepository 조회 퀄리 추가

--- src/main/java/com/team03/monew/subscribe/repository/SubscribeRepository.java
+++ src/main/java/com/team03/monew/subscribe/repository/SubscribeRepository.java
@@ -2,6 +2,8 @@ package com.team03.monew.subscribe.repository;
 
 import com.team03.monew.subscribe.domain.Subscribe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +16,9 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, UUID> {
     Boolean existsByUserIdAndInterestId(UUID userId, UUID interestId);
 
     List<Subscribe> findTop10ByUserIdOrderByCreatedAtDesc(UUID userId);
+
+    @Query("select s.userId from Subscribe s where s.interestId = :interestId")
+    List<UUID> findUserIdsByInterestId(@Param("interestId") UUID interestId);
+
+    List<Subscribe> findByUserIdAndInterestIdIn(UUID userId, List<UUID> interestIds);
 }
