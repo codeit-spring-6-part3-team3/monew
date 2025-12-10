3f45e84e716e0bd6e1adf6775d7fd837b1a3b020
jihoon-68 <pkrwlgns64@gmail.com>
2025-12-10 13:09:12 +0900

    [박지훈] feat : BasicSubscribeService 유저 구독정보 호출 추가

--- src/main/java/com/team03/monew/subscribe/repository/SubscribeRepository.java
+++ src/main/java/com/team03/monew/subscribe/repository/SubscribeRepository.java
@@ -3,6 +3,7 @@ package com.team03.monew.subscribe.repository;
 import com.team03.monew.subscribe.domain.Subscribe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +12,6 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, UUID> {
     Optional<Subscribe> findByUserIdAndInterestId(UUID userId, UUID interestId);
 
     Boolean existsByUserIdAndInterestId(UUID userId, UUID interestId);
+
+    List<Subscribe> findTop10ByUserIdOrderByCreatedAtDesc(UUID userId);
 }
