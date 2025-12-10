3a1771cb5406db3d1981057669df6cc62eac2af9
Hyunwook <hjhsha827@gmail.com>
2025-12-10 16:28:46 +0900

    [이현욱] feat: 관심사 ID로 유저들을 찾는 쿼리 추가

--- src/main/java/com/team03/monew/subscribe/repository/SubscribeRepository.java
+++ src/main/java/com/team03/monew/subscribe/repository/SubscribeRepository.java
@@ -1,14 +1,19 @@
 package com.team03.monew.subscribe.repository;
 
 import com.team03.monew.subscribe.domain.Subscribe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, UUID> {
 
     Optional<Subscribe> findByUserIdAndInterestId(UUID userId, UUID interestId);
 
     Boolean existsByUserIdAndInterestId(UUID userId, UUID interestId);
+
+    @Query("select s.userId from Subscribe s where s.interestId = :interestId")
+    List<UUID> findUserIdsByInterestId(@Param("interestId") UUID interestId);
 }
